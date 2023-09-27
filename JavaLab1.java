3bb271df6a025702b298c057b37a3ed67c70a913
Mandill <145789038+Mandill@users.noreply.github.com>
2023-09-27 11:04:25 +0545

    Add files via upload

---
+++ JavaLab1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javalab1;
+
+/**
+ *
+ * @author Acer
+ */
+public class JavaLab1 {
+
+    public static void main(String[] args) {
+        int[] array={1,2,3,4,5};
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(array[i]);
+        }
+        
+    }
+}
